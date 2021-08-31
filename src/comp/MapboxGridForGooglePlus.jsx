024cbbcff8c56139553699aa8cd5125bbd8bac52
lostman <lunastratos@studiox.co.kr>
2021-08-31 18:51:33 +0900

    추가 add 제작

--- src/comp/MapboxGridForGooglePlus.jsx
+++ src/comp/MapboxGridForGooglePlus.jsx
@@ -2,8 +2,21 @@ import React, { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import * as turf from '@turf/turf';
 import axios from 'axios';
+import tilebelt from '@mapbox/tilebelt';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import SphericalMercator from '@mapbox/sphericalmercator';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+});
+
+// By default, precomputes up to z30
+var sm = new SphericalMercator({
+    size: 256
+});
 
 /**
  * 지도 메인
@@ -12,16 +25,14 @@ export default function MapboxGridForGooglePlus(props) {
 
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(126.812776); // eslint-disable-line no-unused-vars
-    const [lat, setLat] = useState(37.571586); // eslint-disable-line no-unused-vars
-    const [zoom, setZoom] = useState(15); // eslint-disable-line no-unused-vars
-    const [flag, setFlag] = useState(false);
+    const [lngLat, setLngLat] = useState([126.608257, 37.553067]);
+    const [zoom, setZoom] = useState(18); // eslint-disable-line no-unused-vars
     const [start, setStart] = useState(false);
     const [selectPoint, setSelectPoint] = useState();
     const [selectMultiGrid, setSelectMultiGrid] = useState([]);
-    const [count, setCount] = useState(0);
 
     const mapCellIdList = useRef([]);        // 맵상의 셀 아이디 리스트, 맵에 표시되어 있는 셀을 항상 가지고 있어야 함
+
 
 
     const connPlusCodeJsonGrid = async (zoom, x, y) =>{
@@ -51,98 +62,171 @@ export default function MapboxGridForGooglePlus(props) {
         // 중복되는 것들 지우기
         let filteringData = responseData.data.features.filter(item2 => item2.properties.duplicate !== 1);
         responseData.data.features = filteringData;
+        console.log("responseData.data", responseData.data)
         return responseData.data;
         
        }
+
+    const lon2tile = (lon,zoom) => { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+    const lat2tile = (lat,zoom) =>  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+
 
     const initMap = () =>{
         if (map.current) return;
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/outdoors-v11',
-            center: [lng, lat],
+            center: lngLat,
             zoom: zoom,
             scrollZoom: true
         });
 
-        const bounds = map.current.getBounds();
 
-        console.log(`bounds:`, bounds);
-
-
+     
         map.current.on('load', () => {
 
-            //줌이 끝났을때 동작
-            map.current.on('zoomend', function() {
 
-                console.log(map.current.getLayer('mainMap'));
+            map.current.addSource('raster-tiles', {
+                'type': 'raster',
+                'tiles': [
+                'https://grid.plus.codes/grid/wms/19/{x}/{y}.png'
+                ],
+                'tileSize': 256
+                });
+
+
+            
+            console.log(map.current.getSource('raster-tiles'));
+
+            map.current.addLayer(
+                {
+                    'id': 'simple-tiles',
+                    'type': 'raster',
+                    'source': 'raster-tiles',
+                    'minzoom': 0,
+                    'maxzoom': 22,
+                    'paint': {
+                        'raster-opacity': 0.50
+                    }
+                }
+            );
+            
+            const minLat = 37.55171351970851
+            const minLng = 126.6069635197085
+            const maxLng = 37.5544114802915
+            const maxLat = 126.6096614802915
+var bbox = [minLng, minLat, maxLng, maxLat]
+var poly = turf.bboxPolygon(bbox);
+
+console.log(poly)
+
+             map.current.addSource('grid-source', {
+                         'type': "geojson",
+                        'data': poly,
+                        'generateId': true
+            });
+
+            map.current.addLayer(
+                {
+                    'id': 'mainMap',
+                    'type': 'fill',
+                    'source': 'grid-source',
+                    'layout': {},
+                    'paint': {
+                        'fill-color': '#088',
+                        'fill-opacity': 0.5
+                    }
+                }
+            );
+
+            //줌이 끝났을때 동작
+            map.current.on('zoomend', async function() {
 
                 // 줌 레벨 17 이하에서만 동작
-                if(map.current.getZoom() >= 17){
-                    console.log('zoom Level 17 in');
+                if(map.current.getZoom() >= 19){
+                    console.log('zoom Level 19 in');
                    
                     const getCenter =  map.current.getCenter();
+                    const getBound =  map.current.getBounds();
+                    var minLon = getBound.getNorthEast();
+                    var maxLon = getBound.getSouthWest();
+                    const googleY = lat2tile(getCenter.lat, 19);
+                    const googleX = lon2tile(getCenter.lng, 19);
 
-                    const coord = mapboxgl.MercatorCoordinate.fromLngLat({lng: 128, lat: 37}, 0);  
-                    console.log('coord', coord); 
+                    const coordinates = map.current.project([getCenter.lng, getCenter.lat]);
+                    console.log("coordinates", coordinates)
+
+                    const returnData = await connPlusCodeJsonGrid(19, googleX, googleY);
+
                     
-                    if(typeof map.current.getLayer('mainMap') == 'undefined'){
-                        map.current.addSource('grid-source', {
-                            'type': "geojson",
-                            'data': connPlusCodeJsonGrid(19, coord.x, coord.y),
-                            'generateId': true
-                        });
-
-                        map.current.addLayer({
-                            'id': 'mainMap',
-                            'type': 'fill',
-                            'source': 'grid-source',
-                            'layout': {},
-                            'paint': {
-                                'fill-color': '#088',
-                                'fill-opacity': [
-                                    "interpolate", ["linear"], ["get", "density"],
-                                    0, 0.3,
-                                    1, 1
-                                ]
-                            }
-                        });
-
-                        //선택 그리드
-                        map.current.addLayer(
-                            {
-                                'id': 'mainMap-highlighted',
-                                'type': 'fill',
-                                'source': 'grid-source',
-                                'paint': {
-                                    'fill-outline-color': '#484896',
-                                    'fill-color': '#6e599f',
-                                    'fill-opacity': 0.50
-                                },
-                                'filter': ['==', ['get', 'id'], -1]
-                            },
-                            'settlement-label'
-                        );
+                    
 
 
+                    // if(typeof map.current.getLayer('mainMap') == 'undefined'){
 
-                        // 마우스 이벤트 켜기
-                        //map.current.on("click", onClickGridEvent);
 
-                        //지도 안으로 행군하라!
-                        map.current.on('mousemove', function(e) {
-                            var features = map.current.queryRenderedFeatures(e.point, { layers: ['mainMap'] });
-                            if(features[0] != undefined){
-                                const selectIndex = features[0].id;
-                                //                    console.log('A mousemove event has occurred.' + selectIndex);
-                            }
+                    //     map.current.addSource('grid-source', {
+                    //         'type': 'vector',
+                    //         'tiles': [
+                    //         'https://grid.plus.codes/grid/wms/19/{x}/{y}.json?zoomadjust=2'
+                    //         ],
+                    //         'minzoom': 6,
+                    //         'maxzoom': 14
+                    //     });
 
-                        });
-                        //지도 밖으로 행군하라!
-                        map.current.on('mouseover', function() {
-                            console.log('A mouseover event has occurred.');
-                        });
-                    }
+                    //     // map.current.addSource('grid-source', {
+                    //     //     'type': "geojson",
+                    //     //     'data': returnData,
+                    //     //     'generateId': true
+                    //     // });
+
+                    //     map.current.addLayer(
+                    //         {
+                    //             'id': 'mainMap',
+                    //             'type': 'fill',
+                    //             'source': 'grid-source',
+                    //             'layout': {},
+                    //             'paint': {
+                    //                 'fill-color': '#088',
+                    //                 'fill-opacity': 0.50
+                    //             }
+                    //         }
+                    //     );
+
+                    //     //선택 그리드
+                    //     map.current.addLayer(
+                    //         {
+                    //             'id': 'mainMap-highlighted',
+                    //             'type': 'fill',
+                    //             'source': 'grid-source',
+                    //             'paint': {
+                    //                 'fill-outline-color': '#484896',
+                    //                 'fill-color': '#6e599f',
+                    //                 'fill-opacity': 0.50
+                    //             },
+                    //             'filter': ['==', ['get', 'id'], -1]
+                    //         },
+                    //         'settlement-label'
+                    //     );
+
+
+
+                    //     // 마우스 이벤트 켜기
+                    //     //map.current.on("click", onClickGridEvent);
+
+                    //     //지도 안으로 행군하라!
+                    //     map.current.on('mousemove', function(e) {
+                    //         var features = map.current.queryRenderedFeatures(e.point, { layers: ['mainMap'] });
+                    //         if(features[0] != undefined){
+                    //             const selectIndex = features[0].id;
+                    //         }
+
+                    //     });
+                    //     //지도 밖으로 행군하라!
+                    //     map.current.on('mouseover', function() {
+                    //         console.log('A mouseover event has occurred.');
+                    //     });
+                    // }
 
                     setStart(true);
 
@@ -150,19 +234,6 @@ export default function MapboxGridForGooglePlus(props) {
                     // 이벤트 끄기
                     console.log('zoom Level 17 out');
 
-                    //map.current.off('click', onClickGridEvent);
-                    if(typeof map.current.getLayer('mainMap') !== 'undefined') {
-                        map.current.removeLayer('mainMap');
-                    }
-                    if(typeof map.current.getLayer('points') !== 'undefined'){
-                        map.current.removeLayer('points');
-                    }
-                    if(typeof map.current.getLayer('mainMap-highlighted') !== 'undefined'){
-                        map.current.removeLayer('mainMap-highlighted');
-                    }
-                    if(typeof map.current.getSource('grid-source') !== 'undefined') {
-                        map.current.removeSource('grid-source');
-                    }
 
                     setStart(false);
 
@@ -203,8 +274,10 @@ export default function MapboxGridForGooglePlus(props) {
 
     }
 
-
-
+    
+    /**
+     * 시작점
+     */
     useEffect(() => {
         initMap();
         // Clean up on unmount
@@ -276,7 +349,7 @@ export default function MapboxGridForGooglePlus(props) {
                         </button>
                     </div>
                     <div className="sidebar">
-                        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | click : {flag+""}
+                        Longitude: {lngLat[1]} | Latitude: {lngLat[0]} | Zoom: {zoom} 
                     </div>
                     <div ref={mapContainer} className="map-container" style={{height: '100vh'}} />
                 </div>
